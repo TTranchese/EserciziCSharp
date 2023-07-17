9df28097156bbc7278d0d0b24c81bc3051bda201
Tommaso Tranchese <tommaso.tranchese.96@gmail.com>
2023-07-17 15:22:09 +0200

    17/7/2023

---
+++ EseLibriIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EseLibriIdentity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EseLibriIdentity.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public DbSet<Autore> Autori { get; set;}
+    public DbSet<Editore> Editori { get; set;}
+    public DbSet<Libro> Libri { get; set;}
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
