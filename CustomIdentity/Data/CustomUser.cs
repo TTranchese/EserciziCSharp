a917655548da9331a15fb64550b1d202cb7a8bb8
Tommaso Tranchese <tommaso.tranchese.96@gmail.com>
2023-07-17 17:50:47 +0200

    roles

---
+++ CustomIdentity/Data/CustomUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace CustomIdentity.Data;
+
+public class CustomUser: IdentityUser
+{
+    [Required(ErrorMessage = "{0} e' obbligatorio!")]
+    [MaxLength(100, ErrorMessage = "Massimo {1} caratteri!")]
+    public string Nome { get; set; }
+    [Required(ErrorMessage = "{0} e' obbligatorio!")]
+    [MaxLength(100, ErrorMessage = "Massimo {1} caratteri!")]
+    public string Cognome { get; set; }
+}
